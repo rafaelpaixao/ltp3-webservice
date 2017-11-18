76d7f713f732770c15a7b5420e0f2858be95739f
Rafael Paixão <rafaelsspaixao@gmail.com>
2017-11-18 12:24:29 -0300

    reorder fields

--- api/models.py
+++ api/models.py
@@ -9,10 +9,11 @@ YEAR_CHOICES = [(r,r) for r in range(2010, datetime.date.today().year+4)]
 class Turma(models.Model):
 
     disciplina = models.CharField(max_length=100, verbose_name=u"Disciplina")
+    ano = models.IntegerField(choices=YEAR_CHOICES, default=datetime.datetime.now().year, verbose_name=u"Ano")
+    semestre = models.IntegerField(choices=SEMESTER_CHOICES,default=1, verbose_name=u"Semestre")
+
     alunos = models.ManyToManyField('Aluno', blank=True, verbose_name=u"Alunos")
 
-    semestre = models.IntegerField(choices=SEMESTER_CHOICES,default=1, verbose_name=u"Semestre")
-    ano = models.IntegerField(choices=YEAR_CHOICES, default=datetime.datetime.now().year, verbose_name=u"Ano")
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
 
@@ -23,10 +24,11 @@ class Turma(models.Model):
 class Aluno(models.Model):
 
     nome = models.CharField(max_length=100, verbose_name=u"Nome")
+    ano = models.IntegerField(choices=YEAR_CHOICES, default=datetime.datetime.now().year, verbose_name=u"Ano")
+    semestre = models.IntegerField(choices=SEMESTER_CHOICES,default=1, verbose_name=u"Semestre")
+
     turmas = models.ManyToManyField('Turma', through=Turma.alunos.through, blank=True, verbose_name=u"Turmas")
 
-    semestre = models.IntegerField(choices=SEMESTER_CHOICES,default=1, verbose_name=u"Semestre")
-    ano = models.IntegerField(choices=YEAR_CHOICES, default=datetime.datetime.now().year, verbose_name=u"Ano")
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
 
